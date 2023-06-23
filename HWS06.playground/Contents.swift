381294c09931af0f794c20c211486abc97cdfd9f
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-06-23 14:13:07 -0700

    checkpoint 6

---
+++ HWS06.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+struct Car{
+    let model: String
+    let numberOfSeats: Int
+    private(set) var currentGear = 0
+    
+    init(model: String, numberOfSeats: Int){
+        self.model = model
+        self.numberOfSeats = numberOfSeats
+    }
+    
+    mutating func changeGear(newGear: Int){
+        if(newGear < 10 && newGear > 0 && abs(self.currentGear - newGear) == 1){
+            self.currentGear = newGear
+        }else{
+            print("Unable to set gear!!")
+        }
+    }
+}
+
+var chevy = Car(model: "Chevy", numberOfSeats: 5)
+chevy.changeGear(newGear: 1)
+print("New gear is set to \(chevy.currentGear)")
+
+
