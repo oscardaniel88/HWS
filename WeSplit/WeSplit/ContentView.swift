6b56f53ed98ae84e65a6927ea24a4afcd69ea099
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-06-28 13:16:07 -0700

    project 1

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Daniel Camarena on 6/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    var body: some View {
+        NavigationView {
+            Form{
+                Picker("Select your student", selection: $selectedStudent){
+                    ForEach(students, id: \.self){
+                        Text($0)
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
