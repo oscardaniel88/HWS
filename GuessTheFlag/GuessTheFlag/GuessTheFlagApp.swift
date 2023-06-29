c9c13f4b7aedaecde84d16eb6cf67d2ac1f44492
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-06-29 14:23:14 -0700

    Project 2

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Daniel Camarena on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
