fb88124af1b4f50a19b254da38bac7d4e037a81c
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-07-06 13:10:43 -0700

    Project 4 wrap up

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -18,41 +18,35 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
     var body: some View {
         NavigationView{
             Form {
-                VStack(alignment:.leading, spacing:0){
+                Section(header: Text("Wake up").font(.headline)){
                     HStack {
-                        Text("Wake up time").font(.headline)
+                        Text("Choose a time")
                         Spacer()
                         DatePicker("Please enter a time", selection:$wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
                     }
                 }
-                VStack(alignment: .leading, spacing:0){
-                    Text("Desired sleep amount").font(.headline)
+                Section(header: Text("Sleep Amount").font(.headline)){
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step:0.25)
                 }
-                VStack(alignment: .leading, spacing:0){
-                    Text("Daily coffee intake").font(.headline)
+                Section(header: Text("Coffee intake").font(.headline)){
                     Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value:$coffeeAmount, in: 1...20, step: 1)
                 }
-            }
-            .navigationTitle("Better Rest")
-            .toolbar{
-                Button("Calculate", action:calculateBedTime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert){
-                Button("OK") {}
-            }message: {
-                Text(alertMessage)
+                
+                Section(header: Text("Result").font(.headline)){
+                    HStack {
+                        Text("Your ideal bedtime is…")
+                        Spacer()
+                        Text("\(calculateBedTime)").font(.title)
+                    }
+                }
             }
         }
     }
     
-    func calculateBedTime(){
+    var calculateBedTime: String {
         do{
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -61,15 +55,10 @@ struct ContentView: View {
             let minute = (components.minute ?? 0) * 60
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is ..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-            
-            
+            return sleepTime.formatted(date: .omitted, time: .shortened)
         }catch{
-            alertTitle = "Error"
-            alertMessage = "Something went wrong"
+            return ""
         }
-        showingAlert = true
     }
 }
 
