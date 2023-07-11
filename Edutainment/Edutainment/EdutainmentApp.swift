d99c18a26075d1bf568d3991b0740922d4bf05f3
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-07-11 14:37:14 -0700

    wrap up project 4-6

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Daniel Camarena on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
