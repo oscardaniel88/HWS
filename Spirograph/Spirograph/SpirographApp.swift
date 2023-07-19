e55bcf7314df607ff06b2537428191658aa2170b
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-07-18 19:22:52 -0700

    Spirograph

---
+++ Spirograph/Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Daniel Camarena on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
