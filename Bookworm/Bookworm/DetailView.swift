5ac38b4a9d5c18b338fb4c169bba17f5bc4e5b58
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-07-24 23:08:46 -0700

    Fixing crash

--- Bookworm/Bookworm/DetailView.swift
+++ Bookworm/Bookworm/DetailView.swift
@@ -63,20 +63,3 @@ struct DetailView: View {
         dismiss()
     }
 }
-
-struct DetailView_Previews: PreviewProvider {
-    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-    
-    static var previews: some View {
-        let book = Book(context: moc)
-        book.title = "Test book"
-        book.author = "Test author"
-        book.genre = "Fantasy"
-        book.rating = 4
-        book.review = "This was a great book; I really enjoyed it."
-        
-        return NavigationView {
-            DetailView(book: book)
-        }
-    }
-}
