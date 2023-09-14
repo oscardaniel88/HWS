74882293afabefc453383a46cd24bf78b9665586
Daniel Camarena <danielcamarena@Daniels-MacBook-Pro.local>
2023-09-13 20:03:03 -0700

    Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Daniel Camarena on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
