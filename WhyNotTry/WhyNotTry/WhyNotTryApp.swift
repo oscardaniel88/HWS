2bf004b1cb9519ef2449ce97d055a8ed708742d6
oscardaniel88 <oscar.daniel88@gmail.com>
2025-02-20 21:56:06 -0800

    Swift tutorial

---
+++ WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Daniel Camarena on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
